2fea1068a7fbffa8ff8869454e6c5a747c6e86b1
hamim4395 <hamimnurkhamid@amikonm.ac.id>
2022-03-23 22:23:14 +0700

    commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClassanobject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi = new Taxi();
+
+            taxi.DriverName = "jono";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.NumPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClassanobject
+{
+    public class Taxi
+    {
+        //properties
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("DriverNam:{0}", DriverName);
+            Console.WriteLine("OnDuty:{0}", OnDuty);
+            Console.WriteLine("NumPassenge:{0}", NumPassenger);
+        }
+
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0}sedang menjemput penumpang", DriverName);
+        }
+        public void PickUpPassenger()
+        { 
+            Console.WriteLine("{0}selesai mengantar penumpang", DriverName);
+        } 
+
+    }
+}
